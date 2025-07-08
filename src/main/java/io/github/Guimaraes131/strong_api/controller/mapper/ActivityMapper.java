2f8d4cd0f0bffc9c496dcfdef4a570f8fc9df435
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-08 15:19:13 -0300

    feat: add updateFromDTO method in ActivityMapper

--- src/main/java/io/github/Guimaraes131/strong_api/controller/mapper/ActivityMapper.java
+++ src/main/java/io/github/Guimaraes131/strong_api/controller/mapper/ActivityMapper.java
@@ -4,6 +4,7 @@ import io.github.Guimaraes131.strong_api.controller.dto.GetActivityDTO;
 import io.github.Guimaraes131.strong_api.controller.dto.PostActivityDTO;
 import io.github.Guimaraes131.strong_api.model.Activity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ActivityMapper {
@@ -11,4 +12,6 @@ public interface ActivityMapper {
     Activity toEntity(PostActivityDTO dto);
 
     GetActivityDTO toDTO(Activity entity);
+
+    void updateFromDTO(PostActivityDTO dto, @MappingTarget Activity activity);
 }
