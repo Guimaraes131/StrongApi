055d1fcdbcfab8c84045bd1e7cd456b337080256
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-08 11:44:29 -0300

    Starting project

---
+++ src/main/java/io/github/Guimaraes131/strong_api/StrongApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.strong_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrongApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Guimaraes131/strong_api/StrongApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.strong_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
