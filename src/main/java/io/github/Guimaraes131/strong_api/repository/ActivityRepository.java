776d537915f1a2d79e5cf958bcbdb832a380fd48
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-08 17:26:25 -0300

    feat: add findAllByDateBetween method in ActivityRepository

--- src/main/java/io/github/Guimaraes131/strong_api/repository/ActivityRepository.java
+++ src/main/java/io/github/Guimaraes131/strong_api/repository/ActivityRepository.java
@@ -3,7 +3,11 @@ package io.github.Guimaraes131.strong_api.repository;
 import io.github.Guimaraes131.strong_api.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+
+    List<Activity> findAllByDateBetween(LocalDate start, LocalDate end);
 }
