7b053d5aa5d688033e0612e992c2f1a77c810053
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-09 15:55:20 -0300

    feat: add SecurityConfig and CustomUserDetailsService

---
+++ src/main/java/io/github/Guimaraes131/strong_api/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package io.github.Guimaraes131.strong_api.security;
+
+import io.github.Guimaraes131.strong_api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        var user = service.getByLogin(login).orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
+
+        return User.builder()
+                .username(user.getLogin())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[user.getRoles().size()]))
+                .build();
+
+    }
+}
