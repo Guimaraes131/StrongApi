1f9cafa2b335326812f621605e09f24f5a47b26c
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-10 07:55:30 -0300

    feat: add conflictResponse. Static method that returns ErrorResponse

--- src/main/java/io/github/Guimaraes131/strong_api/controller/dto/ErrorResponse.java
+++ src/main/java/io/github/Guimaraes131/strong_api/controller/dto/ErrorResponse.java
@@ -1,7 +1,11 @@
 package io.github.Guimaraes131.strong_api.controller.dto;
 
+import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
 public record ErrorResponse(int status, String message, List<FieldError> errors) {
-
+    public static ErrorResponse conflictResponse(String message) {
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), message, List.of());
+    }
 }
