449312b35dc0ad67b82f2b6def6b7c9eb5bd126d
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-10 07:53:46 -0300

    feat: add existsByLogin in UserRepository and injecting UserValidator in UserService

--- src/main/java/io/github/Guimaraes131/strong_api/service/UserService.java
+++ src/main/java/io/github/Guimaraes131/strong_api/service/UserService.java
@@ -2,6 +2,7 @@ package io.github.Guimaraes131.strong_api.service;
 
 import io.github.Guimaraes131.strong_api.model.User;
 import io.github.Guimaraes131.strong_api.repository.UserRepository;
+import io.github.Guimaraes131.strong_api.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,14 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository repository;
+    private final UserValidator validator;
     private final PasswordEncoder encoder;
 
     public void create(User user) {
         String password = user.getPassword();
 
         user.setPassword(encoder.encode(password));
+        validator.validate(user);
         repository.save(user);
     }
 
