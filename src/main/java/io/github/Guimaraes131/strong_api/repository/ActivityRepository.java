ffcc395b5b951524f9e9a10b4bbe3a242ac358e7
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-10 09:17:16 -0300

    feat: add findByIdAndUser method

--- src/main/java/io/github/Guimaraes131/strong_api/repository/ActivityRepository.java
+++ src/main/java/io/github/Guimaraes131/strong_api/repository/ActivityRepository.java
@@ -1,13 +1,17 @@
 package io.github.Guimaraes131.strong_api.repository;
 
 import io.github.Guimaraes131.strong_api.model.Activity;
+import io.github.Guimaraes131.strong_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ActivityRepository extends JpaRepository<Activity, UUID> {
 
-    List<Activity> findAllByDateBetween(LocalDate start, LocalDate end);
+    List<Activity> findAllByDateBetweenAndUser(LocalDate start, LocalDate end, User user);
+
+    Optional<Activity> findByIdAndUser(UUID id, User user);
 }
